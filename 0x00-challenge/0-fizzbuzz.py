788e2bcdaa45d0ef8e8ab78eaaa3a3491656d410
SIHLE-MTSHALI <sihle.mtshali8@gmail.com>
2024-03-20 23:10:51 -0700

    0 fix

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
+""" FizzBuzz
 """
-FizzBuzz Program: Prints numbers 1 to n, but for multiples of three prints
-"Fizz" instead of the number, for the multiples of five prints "Buzz", and for
-numbers which are multiples of both three and five prints "FizzBuzz".
-"""
-
 import sys
 
+
 def fizzbuzz(n):
-    """Implements the FizzBuzz logic."""
+    """
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
+    """
     if n < 1:
         return
 
+    tmp_result = []
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
-            print("FizzBuzz", end=' ')
+            tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
-            print("Fizz", end=' ')
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
-            print("Buzz", end=' ')
+            tmp_result.append("Buzz")
         else:
-            print(i, end=' ')
-    print()  # New line at the end of the sequence
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
@@ -32,4 +37,3 @@ if __name__ == '__main__':
 
     number = int(sys.argv[1])
     fizzbuzz(number)
-
